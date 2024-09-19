65999a318a94dee1ae20f0ab7bf61341920742b1
Gleb <gleb.bortsov79@gmail.com>
2024-09-19 12:01:04 +1000

    Development:
    * quiz system
    
    Add:
    * text visual

--- Assets/Scripts/Quiz/Question.cs
+++ Assets/Scripts/Quiz/Question.cs
@@ -5,11 +5,13 @@ public class Question : MonoBehaviour
     [Header("PARAMETERS")]
     [Tooltip("Main text")]
     [SerializeField] [TextArea(2, 5)] private string _mainText = "";
+    [SerializeField] private string _rightOptionText = "";
     [SerializeField] [TextArea(2, 5)] private string[] _optionsText = null;
 
     #region PUBLIC FIELDS
 
     public string MainText { get { return _mainText; } set { _mainText = value; } }
+    public string RightOptionText { get { return _rightOptionText; } set { _rightOptionText = value; } }
     public string[] OptionsText { get { return _optionsText; } set { _optionsText = value; } }
 
     #endregion
