c6526fb27de3289a767a32473567fa42028a9e83
Gleb <gleb.bortsov79@gmail.com>
2024-10-15 22:34:07 +1000

    fix

--- Assets/Scripts/YandexMetrika.cs
+++ Assets/Scripts/YandexMetrika.cs
@@ -4,16 +4,16 @@ public class YandexMetrika : MonoBehaviour
 {
     public void OnStartGameStatistic()
     {
-        Application.ExternalEval("ym(98589995, 'reachGoal', 'start_game')");
+        Application.ExternalEval("ym(98633879,'reachGoal','start_game')");
     }
 
     public void OnSuccessQuizStatistic()
     {
-        Application.ExternalEval("ym(98589995,'reachGoal','success_game')");
+        Application.ExternalEval("ym(98633879,'reachGoal','success_game')");
     }
 
     public void OnFailQuizStatistic()
     {
-        Application.ExternalEval("ym(98589995,'reachGoal','fail_game')");
+        Application.ExternalEval("ym(98633879,'reachGoal','fail_game')");
     }
 }
